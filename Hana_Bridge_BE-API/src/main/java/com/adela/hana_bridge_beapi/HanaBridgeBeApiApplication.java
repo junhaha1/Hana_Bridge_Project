f89cac86c920432247b7d9ec05ad991d53c8683a
junhaha1 <wnsgk919@naver.com>
2025-04-22 17:12:49 +0900

    first commit

---
+++ Hana_Bridge_BE-API/src/main/java/com/adela/hana_bridge_beapi/HanaBridgeBeApiApplication.java
@@ -0,0 +1,13 @@
+package com.adela.hana_bridge_beapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanaBridgeBeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanaBridgeBeApiApplication.class, args);
+	}
+
+}
---
+++ Hana_Bridge_BE-API/src/test/java/com/adela/hana_bridge_beapi/HanaBridgeBeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adela.hana_bridge_beapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanaBridgeBeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
