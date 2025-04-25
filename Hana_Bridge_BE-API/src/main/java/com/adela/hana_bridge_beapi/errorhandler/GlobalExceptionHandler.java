d7b40c7f0cda7107d51e089fa5dc3da35dab1ef5
junhaha1 <wnsgk919@naver.com>
2025-04-25 13:11:44 +0900

    Feat: 사용자 API 추가 구현 - 로그인 요청 dto 유효성 검증 어노테이션 추가 - 유효성 검사 시 에러 처리 핸들러 메소드 추가

--- Hana_Bridge_BE-API/src/main/java/com/adela/hana_bridge_beapi/errorhandler/GlobalExceptionHandler.java
+++ Hana_Bridge_BE-API/src/main/java/com/adela/hana_bridge_beapi/errorhandler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.adela.hana_bridge_beapi.errorhandler;
 
 import com.adela.hana_bridge_beapi.dto.error.ErrorResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,5 +12,12 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleIllegalArgument(IllegalArgumentException ex) {
         return ResponseEntity.badRequest()
                 .body(new ErrorResponse("BAD_REQUEST", ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidation(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult().getFieldError().getDefaultMessage();
+        return ResponseEntity.badRequest()
+                .body(new ErrorResponse("VALIDATION_FAILED", errorMessage));
     }
 }
