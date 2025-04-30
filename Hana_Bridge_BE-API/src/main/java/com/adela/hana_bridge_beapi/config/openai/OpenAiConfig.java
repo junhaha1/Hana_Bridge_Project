2012ffcdce49096f9cee6dc8b9cb9792aaba3132
junhaha1 <wnsgk919@naver.com>
2025-04-30 11:22:11 +0900

    Feat: OpenAI Controller, Service 구현 - 불필요한 주석 제거 - 타임아웃 시간 변경

--- Hana_Bridge_BE-API/src/main/java/com/adela/hana_bridge_beapi/config/openai/OpenAiConfig.java
+++ Hana_Bridge_BE-API/src/main/java/com/adela/hana_bridge_beapi/config/openai/OpenAiConfig.java
@@ -2,7 +2,6 @@ package com.adela.hana_bridge_beapi.config.openai;
 
 
 import io.netty.channel.ChannelOption;
-import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,14 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.apache.hc.client5.http.config.RequestConfig;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.core5.util.Timeout;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
 import reactor.netty.http.client.HttpClient;
-import reactor.netty.tcp.TcpClient;
 
 import java.util.concurrent.TimeUnit;
 
@@ -30,14 +22,14 @@ public class OpenAiConfig {
     private String openAiKey;
 
     @Bean
-    public WebClient OpenAiWebClient(){
+    public WebClient openAiWebClient(){
         //타임 아웃 설정
         HttpClient httpClient = HttpClient.create()
                 //ChatGPT 서버 연결 시간 5초 이내
                 .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
                 .doOnConnected(conn -> conn
                         //ChatGPT 응답 시간 10초 이내
-                        .addHandlerLast(new ReadTimeoutHandler(10000, TimeUnit.MILLISECONDS))
+                        .addHandlerLast(new ReadTimeoutHandler(15000, TimeUnit.MILLISECONDS))
                         //요청을 ChatGPT 서버로 보내는 시간 5초 이내
                         .addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS)));
 
@@ -48,27 +40,4 @@ public class OpenAiConfig {
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .build();
     }
-//    @Bean
-//    public RestTemplate template(){
-//        RequestConfig config = RequestConfig.custom()
-//                .setConnectionRequestTimeout(Timeout.ofSeconds(5))
-//                .setResponseTimeout(Timeout.ofSeconds(10))
-//                .build();
-//
-//        CloseableHttpClient httpClient = HttpClients.custom()
-//                .setDefaultRequestConfig(config)
-//                .build();
-//
-//        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
-//
-//        RestTemplate restTemplate = new RestTemplate(requestFactory);
-//        restTemplate.getInterceptors().add((request, body, execution) -> {
-//            request.getHeaders().add("Authorization", "Bearer " + openAiKey);
-//            request.getHeaders().setContentType(MediaType.APPLICATION_JSON);
-//            return execution.execute(request, body);
-//        });
-//        return restTemplate;
-//    }
-
-
 }
