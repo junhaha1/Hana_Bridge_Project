e5cd85978059de7671221c8805c1f0593b6885a4
junhaha1 <wnsgk919@naver.com>
2025-05-07 14:20:58 +0900

    Feat: 질문 연관성에 따른 게시글 생성

---
+++ Hana_Bridge_BE-API/src/main/java/com/adela/hana_bridge_beapi/dto/openai/ClientSummaryRequest.java
@@ -0,0 +1,16 @@
+package com.adela.hana_bridge_beapi.dto.openai;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClientSummaryRequest {
+    private int promptLevel;
+    private String totalContent;
+    private String coreContent;
+}
