f9c4a83b9ab73f0fb1b794604fc3e5f115e73f3d
junhaha1 <wnsgk919@naver.com>
2025-05-19 16:51:59 +0900

    Refactor: Code게시판 세부 디자인 클래스명 조합 정리 - leftheader 구조 변경

---
+++ Hana_Bridge_FE-Cli/src/style/CommonBoardStyle.jsx
@@ -0,0 +1,24 @@
+/* 상단 게시판 타이틀*/
+export const mainTitle = "w-1/2 text-left font-bold text-white";
+
+/* 검색창 스타일 */
+export const searchBox = "flex pl-5 py-2 rounded-full bg-white text-black placeholder-gray-400 shadow";
+export const inputBox = "bg-transparent focus:outline-none";
+
+/*글 작성 버튼 */
+export const addButton = "bg-white hover:!bg-[#C5BCFF] hover:text-black px-4 py-2 rounded-md text-sm font-semibold text-indigo-900 transition-colors duration-300";
+
+/*정렬 체크박스*/
+export const sortCheckLayout = "flex gap-3 justify-end mt-4 mb-2";
+export const sortCheckBox = "text-white pr-1 py-2 text-sm bg-transparent rounded-md focus:outline-none focus:ring-0 focus:border-transparent";
+
+/* Card 게시글 위치*/
+export const cardTopLayout = "flex justify-between items-start";
+export const cardBottomLayout = "flex justify-between gap-4";
+
+/* Card 게시글 내부 요소*/
+export const cardTitle = "text-white text-lg font-semibold line-clamp-1";
+export const cardContent = "text-sm text-gray-200 line-clamp-1";
+export const cardAuthor = "flex text-purple-300 gap-1";
+export const cardGood = "text-indigo-300 flex items-center";
+export const cardComment = "text-gray-300 flex items-center";
