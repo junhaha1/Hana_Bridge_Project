fe55d792fa7107475d3be806096a2e3413d33e9b
junhaha1 <wnsgk919@naver.com>
2025-05-19 15:50:29 +0900

    Feat: Code게시판 세부 디자인 공통 적용

--- Hana_Bridge_FE-Cli/src/style/CommonStyle.jsx
+++ Hana_Bridge_FE-Cli/src/style/CommonStyle.jsx
@@ -3,6 +3,6 @@ import "../css/Scroll.css";
 //scroll 
 export const scrollStyle = "custom-scroll overflow-y-auto";
 //card 
-export const cardStyle = "border border-white/30 bg-white/5 backdrop-blur-sm rounded-md p-6 shadow-md hover:shadow-lg hover:bg-white/15 transition duration-200";
+export const cardStyle = "border-t border-b border-white/30 backdrop-blur-sm p-6 cursor-pointer hover:shadow-lg hover:bg-white/15 transition duration-200";
 //button
 export const buttonStyle = "rounded cursor-pointer no-underline";
