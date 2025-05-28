70b312dfbc0ce4e61c0048ede49cacc2f97063ce
sonyoona <sonyoo0526@naver.com>
2025-05-28 17:06:41 +0900

    home 화면 test3

--- Hana_Bridge_FE-Cli/src/style/CommonHomeStyle.jsx
+++ Hana_Bridge_FE-Cli/src/style/CommonHomeStyle.jsx
@@ -1,10 +1,10 @@
-export const sectionLayout = "w-screen h-screen flex max-y-[400px]:flex-col flex-row items-center justify-center max-y-[400px]:gap-4 gap-6 text-white max-y-[400px]:px-4 px-6";
+export const sectionLayout = "w-screen h-screen flex max-w-[400px]:flex-col flex-row items-center justify-center max-w-[400px]:gap-4 gap-6 text-white max-w-[400px]:px-4 px-6";
 export const sectionBox = "flex flex-col items-center justify-center text-center";
 
-export const homeTitle = "max-y-[400px]:text-base max-y-[400px]:font-semibold text-5xl font-extrabold max-y-[400px]:mb-6 mb-8 leading-tight";
-export const homeText = "max-y-[400px]:text-base text-xl max-y-[400px]:mb-4 mb-8";
-export const homeButton = "cursor-pointer no-underline bg-indigo-500 hover:bg-indigo-600 text-white font-semibold py-3 px-6 rounded-full max-y-[400px]:test-base text-lg";
-export const homeAnimation = "max-y-[400px]:w-[280] max-y-[400px]:h-[280px] w-[440px] h-[440px]";
+export const homeTitle = "max-w-[400px]:text-base max-y-[400px]:font-semibold text-5xl font-extrabold max-w-[400px]:mb-6 mb-8 leading-tight";
+export const homeText = "max-w-[400px]:text-base text-xl max-w-[400px]:mb-4 mb-8";
+export const homeButton = "cursor-pointer no-underline bg-indigo-500 hover:bg-indigo-600 text-white font-semibold py-3 px-6 rounded-full max-w-[400px]:test-base text-lg";
+export const homeAnimation = "max-w-[400px]:w-[280] max-w-[400px]:h-[280px] w-[440px] h-[440px]";
 
 // 모바일: column / ≥ md: row
 //export const sectionLayout =  "w-full h-full flex flex-col md:flex-row items-center justify-center gap-4 md:gap-6 text-white px-4 md:px-6";
