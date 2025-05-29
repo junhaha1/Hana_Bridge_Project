ad383c25d0bf21832139c123342b10895c4e3c25
sonyoona <sonyoo0526@naver.com>
2025-05-29 10:11:54 +0900

    home화면, top헤더 수정

--- Hana_Bridge_FE-Cli/src/style/CommonHeaderStyle.jsx
+++ Hana_Bridge_FE-Cli/src/style/CommonHeaderStyle.jsx
@@ -1,11 +1,13 @@
 export const titleBox = "flex items-center no-underline cursor-pointer";
-export const serviceBox = "flex items-center gap-4";
+export const serviceBox = "flex items-center gap-1";
 
-export const mainTitle = "text-[50px] text-white font-bold no-underline";
+export const mainTitle = "text-[50px] max-md:text-[30px] text-white font-bold no-underline";
 
 export const userButton = "flex items-center text-white text-lg text-[20px] p-2 no-underline hover:!text-gray-400 hover:underline"
-export const commonButton = "text-white text-lg p-2 rounded-md no-underline hover:!text-gray-400";
+export const commonButton = "text-white text-lg max-md:text-sm p-2 max-md:p-0 rounded-md no-underline hover:!text-gray-400";
 export const addButton = "text-white text-lg p-2 rounded-md no-underline hover:!text-green-300";
 export const logoutButton = "text-white text-lg p-2 rounded-md no-underline hover:!text-red-300";
 
 export const userIcon = "w-[25px] h-[25px] text-white mr-2 mt-1";
+
+//max-md, 사용
--- Hana_Bridge_FE-Cli/src/style/CommonHomeStyle.jsx
+++ Hana_Bridge_FE-Cli/src/style/CommonHomeStyle.jsx
@@ -1,7 +1,9 @@
-export const sectionLayout = "w-screen h-screen flex flex-row items-center justify-center max-md:flex-col max-md:gap-4 gap-6 text-white max-md:px-4 px-6";
+export const sectionLayout = "w-screen h-screen flex  flex-col md:flex-row items-center justify-center max-md:gap-4 gap-6 text-white px-6 max-md:px-4 max-md: translate-y-20";
 export const sectionBox = "flex flex-col items-center justify-center text-center";
 
-export const homeTitle = "text-5xl font-extrabold mb-8 leading-tight max-md:text-2xl max-md:font-semibold max-md:mb-6";
-export const homeText = "text-xl mb-8 max-md:text-base max-md:mb-4";
+export const homeTitle = "text-5xl font-extrabold mb-8 leading-tight max-md:text-2xl max-md:font-semibold max-md:mb-3";
+export const homeText = "text-xl mb-8 max-md:text-base max-md:mb-3";
 export const homeButton = "bg-indigo-500 no-underline hover:bg-indigo-600 text-white font-semibold py-3 px-6 rounded-full text-lg max-md:py-1 max-md:px-3 max-md:text-xs";
-export const homeAnimation = "w-[440px] h-[440px] max-md:w-[280px] max-md:h-[280px]";
+export const homeAnimation = "w-[440px] h-[440px] max-md:w-[250px] max-md:h-[250px]";
+
+//max-md, md 사용
