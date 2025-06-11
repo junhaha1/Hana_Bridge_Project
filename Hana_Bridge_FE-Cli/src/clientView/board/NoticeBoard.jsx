8b0278bd453d9fc7702da12ba359bcde6629a5e6
sonyoona <sonyoo0526@naver.com>
2025-06-11 16:29:43 +0900

    notice board 간격 조절

--- Hana_Bridge_FE-Cli/src/clientView/board/NoticeBoard.jsx
+++ Hana_Bridge_FE-Cli/src/clientView/board/NoticeBoard.jsx
@@ -219,7 +219,7 @@ const NoticeBoard = () => {
   return (
     <>
     <div ref={scrollRef} className={scrollStyle +" max-md:h-[65vh] md:h-[90vh] mt-1 ml-20 pr-40 max-md:m-1 max-md:p-2 max-md:overflow-x-hidden"}>
-      <div className="flex justify-between p-1 md:mt-11 max-md:flex-col">
+      <div className="flex justify-between p-1 md:mt-11 max-md:flex-col mb-2">
         <h3 className={mainTitle}>공지 게시판</h3>
         <div className="w-1/2 flex justify-end gap-6 max-md:w-full">
           <div className={searchBox}>
