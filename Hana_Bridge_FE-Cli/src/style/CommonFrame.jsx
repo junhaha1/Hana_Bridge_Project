e963a48359b0ba2fb263a6d643b5118c7fccf8c3
sonyoona <sonyoo0526@naver.com>
2025-06-20 17:29:19 +0900

    스크롤 문제 고쳐야함

--- Hana_Bridge_FE-Cli/src/style/CommonFrame.jsx
+++ Hana_Bridge_FE-Cli/src/style/CommonFrame.jsx
@@ -3,5 +3,5 @@ export const headerFrame = "fixed w-full h-[80px] max-md:h-[50px] px-10 max-md:p
 export const leftFrame = "w-1/5 max-md:w-full md:h-full ml-40 max-md:m-2 pr-10 max-md:pr-2 bg-transparent text-white border-r border-white/20 ";
 export const helperFrame = "fixed bottom-0 right-10 z-[8000]";
 export const boardFrame = "";
-export const detailFrame = "w-4/5 overflow-x-hidden overflow-y-hidden max-h-screen max-w-full max-md:w-full text-white flex-col item-center";
+export const detailFrame = "w-4/5 overflow-x-hidden md:h-[calc(100vh-80px)] max-md:h-[calc(100vh-50px)] max-w-full max-md:w-full text-white flex-col item-center";
 export const homeFrame = "custom-scroll w-screen h-screen overflow-x-hidden overflow-y-scroll scroll-smooth bg-zinc-800";
